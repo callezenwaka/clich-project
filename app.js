470f818565288af26160da4a370e0aaeeff96299
callezenwaka <callisezenwaka@gmail.com>
2018-09-18 13:43:47 +0100

    first commit

--- app.js
+++ app.js
@@ -24,13 +24,6 @@ app.use(cors({
   credentials: false
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS');
-  next();
-});
-
 app.use('/cliches', clichRoutes);
 
 
